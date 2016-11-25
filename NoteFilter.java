003db5b07d4438f50a09b78e0340105a76ba106e
lgtonga <haianyidong@sina.com>
2016-11-25 16:58:12 +0800

    notetest

---
+++ NoteFilter.java
@@ -0,0 +1,15 @@
+package com.isoftstone.sxy.pm.test.git;
+/**
+ * 注释过滤
+* @ClassName: NoteFilter    
+* @Description: 
+* @author tlg  
+* @date 2016年11月25日 下午4:20:16    
+*
+ */
+//zhushi
+public interface NoteFilter {
+	//test
+	public int filter(String content);
+	//test2
+}
